36394cde11af536918686881f1ade2858aca8399
urbanhooker <lznhdp@gmail.com>
2016-11-30 11:48:31 +0800

    1.project init

---
+++ ExperienceCluster/ExperienceCluster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExperienceCluster
+//
+//  Created by Roy Miller on 16/11/30.
+//  Copyright © 2016年 Roy Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExperienceCluster/ExperienceCluster/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExperienceCluster
+//
+//  Created by Roy Miller on 16/11/30.
+//  Copyright © 2016年 Roy Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
