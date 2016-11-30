73c3a8ce356be558c0830ca3f245dd7b9cd57a11
urbanhooker <lznhdp@gmail.com>
2016-11-30 16:39:54 +0800

    1. Add button

--- ExperienceCluster/ExperienceCluster/Category/UIView+Dot.h
+++ ExperienceCluster/ExperienceCluster/Category/UIView+Dot.h
@@ -9,5 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface UIView (Dot)
-
+@property (nonatomic) CGFloat left;
+@property (nonatomic) CGFloat top;
+@property (nonatomic) CGFloat right;
+@property (nonatomic) CGFloat bottom;
+@property (nonatomic) CGFloat width;
+@property (nonatomic) CGFloat height;
+@property (nonatomic) CGFloat centerX;
+@property (nonatomic) CGFloat centerY;
+@property (nonatomic) CGPoint origin;
+@property (nonatomic) CGSize  size;
 @end
--- ExperienceCluster/ExperienceCluster/Cluster/BuildInterfaceController.m
+++ ExperienceCluster/ExperienceCluster/Cluster/BuildInterfaceController.m
@@ -22,8 +22,41 @@
     _labelMadeByCode = [UILabel new];
     NSAttributedString *attr = [[NSAttributedString alloc] initWithString:@"Hi there, I'm the label made by code, and the button below are the bodies you can click to test the result." attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:20], NSForegroundColorAttributeName:[UIColor darkGrayColor]}];
     _labelMadeByCode.attributedText = attr;
-    
+    _labelMadeByCode.numberOfLines = 0;
+    _labelMadeByCode.size = CGSizeMake(200, 1000);
+    [_labelMadeByCode sizeToFit];
+    _labelMadeByCode.center = self.view.center;
+    _labelMadeByCode.centerY -= 100;
     [self.view addSubview:_labelMadeByCode];
+    
+    _btnPushToStoryboard = [[UIButton alloc] initWithFrame:(CGRect){.size = CGSizeMake(100, 50)}];
+    _btnPushToStoryboard.centerX = _labelMadeByCode.centerX;
+    _btnPushToStoryboard.top = _labelMadeByCode.bottom+50;
+    [_btnPushToStoryboard setTitle:@"Push ME!" forState:UIControlStateNormal];
+    [_btnPushToStoryboard addTarget:self action:@selector(pushToStoryboard:) forControlEvents:UIControlEventTouchUpInside];
+    _btnPushToStoryboard.tag = 0;
+    [self.view addSubview:_btnPushToStoryboard];
+
+    
+    _btnMadeByXib = [[[NSBundle mainBundle] loadNibNamed:@"View" owner:nil options:nil] lastObject];
+    _btnMadeByXib.size = CGSizeMake(200, 50);
+    _btnMadeByXib.backgroundColor = [UIColor brownColor];
+    _btnMadeByXib.centerX = _btnPushToStoryboard.centerX;
+    _btnMadeByXib.top = _btnPushToStoryboard.bottom+50;
+    [_btnMadeByXib addTarget:self action:@selector(pushToStoryboard:) forControlEvents:UIControlEventTouchUpInside];
+    _btnMadeByXib.tag = 1;
+    [self.view addSubview:_btnMadeByXib];
+
 }
+
+- (void)pushToStoryboard:(UIButton *)button {
+    if (button.tag == 0) {
+        
+    }
+    else if (button.tag == 1) {
+        NSLog(@"Hi, i'm xib");
+    }
+}
+
 
 @end
